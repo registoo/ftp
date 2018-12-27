a493ec36669a38ad2a8bb74f9af369ce68e886b6
REGISTOO <registoooqweqwe@gmail.com>
2018-12-27 22:12:22 +0300

    add server express side

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const search = require('./src/getFiles.js')
 
+app.get('/', function (req, res) {
+  res.send(search(__dirname));
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
