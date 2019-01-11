66de8fd480be3fba1388f5569722876cd982a974
note_book <registoooqweqwe@gmail.com>
2019-01-11 15:04:55 +0300

    NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,38 @@
 const path = require('path');
-const NODE_ENV = process.env.NODE_ENV || 'development';
 
-module.exports = {
-  entry: './src/testReact.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  node: {
-   fs: "empty"
-  },
-  devtool: NODE_ENV === 'development' ? 'source-map' : false,
-  watch: NODE_ENV === 'development',
-  watchOptions: {
-    aggregateTimeout: 200,
-    ignored: /node_modules/,
-    poll: 1000
-  },
-  module: {
-    rules: [{
-      test: /\.js?$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        // Options to configure the babel. here we have set up the preset. this can be replaced with .babelrc file
-        options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"]
+module.exports = (env, options) => {
+  const NODE_ENV = options.mode
+  return {
+    entry: {
+      testReact: './src/testReact.js'
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+      library: '[name]'
+    },
+    node: {
+     fs: "empty"
+    },
+    devtool: NODE_ENV === 'development' ? 'source-map' : false,
+    watch: NODE_ENV === 'development',
+    watchOptions: {
+      aggregateTimeout: 200,
+      ignored: /node_modules/,
+      poll: 1000
+    },
+    module: {
+      rules: [{
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          // Options to configure the babel. here we have set up the preset. this can be replaced with .babelrc file
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
         }
-      }
-    }]
+      }]
+    }
   }
 };
