ae9696dbe64ccc890aeb3d43c728d27b16717709
Unknown <registoooqweqwe@gmail.com>
2019-01-14 07:16:39 +0300

    some updates

--- src/getFiles.js
+++ src/getFiles.js
@@ -27,3 +27,17 @@ const search = (base) => { // base should be a string
 }
 
 module.exports = search
+
+
+const React = require('react')
+const {render} = require('react-dom')
+function HelloWorld(){
+  return (
+    <div>
+      <h1>привет мир </h1>
+    </div>
+  )
+}
+
+render(<HelloWorld />, document.getElementById('root'))
+module.exports.welcome = alert('привет мир !!!')
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,39 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/testReact.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  node: {
-   fs: "empty"
-  },
-  devtool: 'cheap-source-map',
-  watch: true,
-  watchOptions: {
-    aggregateTimeout: 200,
-    ignored: /node_modules/,
-    poll: 1000
-  },
-  module: {
-    rules: [{
-      test: /\.js?$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        // Options to configure the babel. here we have set up the preset. this can be replaced with .babelrc file
-        options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"]
+module.exports = (env, options) => {
+  const NODE_ENV = options.mode
+  return {
+    entry: {
+      testReact: './src/testReact.js'
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+      publicPath: '/',
+      library: '[name]'
+    },
+    node: {
+     fs: "empty"
+    },
+    devtool: NODE_ENV === 'development' ? 'source-map' : false,
+    watch: NODE_ENV === 'development',
+    watchOptions: {
+      aggregateTimeout: 200,
+      ignored: /node_modules/,
+      poll: 1000
+    },
+    module: {
+      rules: [{
+        test: /\.js?$/,
+        include: [path.resolve(__dirname, 'src')],
+        use: {
+          loader: 'babel-loader',
+          // Options to configure the babel. here we have set up the preset. this can be replaced with .babelrc file
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
         }
-      }
-    }]
-  }
-};
+      }]
+    }
+  };
+}
