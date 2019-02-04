f98ff37602356a484f78d36e4d24b9cbe44335c2
note_book <registoooqweqwe@gmail.com>
2019-02-04 16:40:15 +0300

    04.02.19
    ноут

--- src/testReact.js
+++ src/testReact.js
@@ -1,57 +1,41 @@
 const path = require("path");
 const fs = require("fs");
-const files = require("./files.json").files;
-
-const f = () => {
-  const z = Object.assign(files);
-  console.log(Object.getOwnPropertyNames(z));
-};
-
-const obj = {};
-
-function separator(qwe) {
+const fi = require("./serverSide/getFiles");
+const files = fi.flatten(__dirname);
+function separator(mustBeArray) {
+  function addFile(elem, targetObj) {
+    if (targetObj.hasOwnProperty("files") && Array.isArray(targetObj.files)) {
+      targetObj.files.push(elem);
+      return;
+    } else if (!obj.hasOwnProperty("files")) {
+      targetObj.files = [];
+      targetObj.files.push(elem);
+      return;
+    }
+    throw new Error("Files not an array");
+  }
+  const callback = err => {
+    if (err) throw err;
+  };
   function innerMap(elem) {
+    const obj = {};
     const separatePathToArr = elem.split(path.sep);
     const file = separatePathToArr.slice().pop();
     const dir = separatePathToArr.slice(0, -1);
-    ff(dir, file, elem);
+    if (dir.length === 0) {
+      addFile(file, obj);
+      return;
+    }
+    const pathToFile = dir.reduce(
+      (obj, key) => (obj = obj[key] = obj[key] || {}),
+      obj
+    );
+    pathToFile.files = pathToFile.files || [];
+    addFile(elem, pathToFile);
+    return obj;
   }
-  qwe.map(innerMap);
+  mustBeArray.map(innerMap);
 }
-
-separator(files);
-
-const createObjProp = (obj, elem) => {
-  return obj.elem;
-};
-
-function ff(dir, file, elem) {
-  if (dir.length === 0) {
-    addFile(file, obj);
-    return;
-  }
-  const pathToFile = dir.reduce(
-    (obj, key) => (obj = obj[key] = obj[key] || {}),
-    obj
-  );
-  pathToFile.files = pathToFile.files || [];
-  addFile(elem, pathToFile);
-}
-
-function addFile(elem, targetObj) {
-  if (targetObj.hasOwnProperty("files") && Array.isArray(targetObj.files)) {
-    targetObj.files.push(elem);
-    return;
-  } else if (!obj.hasOwnProperty("files")) {
-    targetObj.files = [];
-    targetObj.files.push(elem);
-    return;
-  }
-  throw new Error("Files not an array");
-}
-console.log(obj);
-const callback = err => {
-  if (err) throw err;
-};
-
-fs.writeFile("src/filesForReact.json", JSON.stringify(obj, false, 2), callback);
+const a = separator(__dirname);
+console.logcmd(a);
+// fs.writeFile("src/filesForReact.json", JSON.stringify(obj, false, 2), callback);
