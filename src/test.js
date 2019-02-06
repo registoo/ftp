90405388e8243784405c70aba80319f33647484e
note_book <registoooqweqwe@gmail.com>
2019-02-06 16:25:45 +0300

    06.02.19
    ноут

--- src/test.js
+++ src/test.js
@@ -1,19 +1,24 @@
 const files = require("./files.json");
 const React = require("react");
 const { render } = require("react-dom");
-
+const path = require("path");
 function FTPfiles(props) {
-  console.log(props.startDir);
   const arr = Object.getOwnPropertyNames(props.startDir);
   if (arr.length == 0) {
     return null;
   }
-  const a = arr.map((elem, id) => {
+  let id = 0;
+  const a = arr.map(elem => {
     if (elem === "files") {
       return null;
     }
-    return <DirFile dirName={elem} dirObj={props.startDir[elem]} key={id} />;
+    const result = (
+      <DirFile dirName={elem} dirObj={props.startDir[elem]} key={id} />
+    );
+    id += 1;
+    return result;
   });
+  a.push(<DivFiles arrOfElem={props.startDir.files} key={id} />);
   return a;
 }
 class DirFile extends React.Component {
@@ -26,24 +31,21 @@ class DirFile extends React.Component {
   handlerButtonValue() {
     this.setState({ filesButton: !this.state.filesButton });
   }
-  fu() {
+  render() {
     return (
-      <div className="flexbox">
-        <div className="leftCol">
-          <button onClick={() => this.handlerButtonValue()}>
-            {this.state.filesButton ? "+" : "-"}
-          </button>
+      <div>
+        <button onClick={() => this.handlerButtonValue()}>
+          {this.state.filesButton ? "+" : "-"}
+        </button>
+        {this.props.dirName}
+        <div className="childMargin">
           <FilesFromButton
             filesButton={this.state.filesButton}
             dirObj={this.props.dirObj}
           />
         </div>
-        <div className="rightCol">{this.props.dirName}</div>
       </div>
     );
-  }
-  render() {
-    return <div>{this.fu()}</div>;
   }
 }
 
@@ -56,6 +58,15 @@ function FilesFromButton(props) {
       <FTPfiles startDir={props.dirObj} />
     </div>
   );
+}
+
+function DivFiles(props) {
+  const a = props.arrOfElem.map((elem, id) => {
+    const separatePathToArr = elem.split(path.sep);
+    console.log("foo\\bar\\baz".split(path.sep));
+    return <div key={id}>{elem}</div>;
+  });
+  return a;
 }
 
 render(<FTPfiles startDir={files} />, document.getElementById("root"));
