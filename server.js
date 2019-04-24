ebe1ce8517104e7e5136895cd5603e630a4d9f74
note_book <registoooqweqwe@gmail.com>
2019-04-24 16:36:38 +0300

    Изменение ответа сервера red button

--- server.js
+++ server.js
@@ -1,36 +1,34 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const fs = require("fs");
-const path = require("path");
-const hashWatcher = require("./src/serverSide/hashWatcher");
-const initJsonHash = require("./src/serverside/initJsonHash");
-const WebSocket = require("ws").Server;
-const JSONchange = require("./src/serverSide/JSONchangeEmitter");
-const bodyParser = require("body-parser");
+const fs = require('fs');
+const path = require('path');
+const hashWatcher = require('./src/serverSide/hashWatcher');
+const initJsonHash = require('./src/serverside/initJsonHash');
+const WebSocket = require('ws').Server;
+const JSONchange = require('./src/serverSide/JSONchangeEmitter');
+const bodyParser = require('body-parser');
 
-const serveDir = "dist";
+const serveDir = 'dist';
 // делает абсолютный путь к папке
 const serveFullDir = path.join(__dirname, serveDir);
 
 async function mainFunction(dir) {
   // инициализирует JSON с хэшами файлов в папке dir
-  await initJsonHash(dir, "SHA1");
+  await initJsonHash(dir, 'SHA1');
   // ставит вотчера на папку dir
   hashWatcher(dir);
 }
 
 mainFunction(serveFullDir);
-const textParser = bodyParser.text({ type: "plaint/text" });
+const textParser = bodyParser.text({ type: 'plaint/text' });
 
-app.post("/checkfronthash", textParser, function(req, res) {
-  console.log(req.body);
-  res.send("qwe");
+app.post('/checkfronthash', textParser, function(req, res) {
+  console.log('body: ', req.body);
+  res.send('Ответ сервера');
 
-  JSONchange.on("change", e => {
+  JSONchange.on('change', e => {
     if (e) console.log(`JSONchange error:\r\nconsole.log(e)`);
 
-    // res.send("qwe");
-    // res.end();
     return;
   });
 });
@@ -43,5 +41,5 @@ try {
 app.use(express.static(serveFullDir));
 
 app.listen(3000, function() {
-  console.log("Example app listening on port 3000!");
+  console.log('Example app listening on port 3000!');
 });
