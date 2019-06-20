2de35ae676b17462a0a0e4454bd2b120aa29a4f4
QQ <Ilya21@protonmail.com>
2019-06-20 21:18:49 +0300

    ничего не добавлено
    стилистическое изменение

--- src/clientSide/isVideo.js
+++ src/clientSide/isVideo.js
@@ -1,21 +1,29 @@
-const _ = require("lodash");
-const React = require("react");
-const { render } = require("react-dom");
+const _ = require('lodash');
+const React = require('react');
+const { render } = require('react-dom');
 
 module.exports = (path, fileName) => {
-  const extAll = _.split(fileName, ".");
+  const extAll = _.split(fileName, '.');
   const ext = extAll[extAll.length - 1];
   switch (ext) {
-    case "mp4":
+    case 'mp4':
       return (
         <video controls src={path} type="video/mp4">
           I'm sorry; your browser doesn't support HTML5 video in MP4 with H.264.
         </video>
       );
       break;
-    case "webm":
+    case 'webm':
       return (
         <video controls src={path} type="video/webm">
+          I'm sorry; your browser doesn't support HTML5 video in WebM with
+          VP8/VP9.
+        </video>
+      );
+      break;
+    case 'mkv':
+      return (
+        <video controls src={path} type="video/x-matroska, audio/x-matroska">
           I'm sorry; your browser doesn't support HTML5 video in WebM with
           VP8/VP9.
         </video>
