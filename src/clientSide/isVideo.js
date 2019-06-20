b81a1f24a1b08ee0462237b3531de8f4f9294123
QQ <Ilya21@protonmail.com>
2019-06-20 22:04:00 +0300

    add videojs

--- src/clientSide/isVideo.js
+++ src/clientSide/isVideo.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 const React = require('react');
 const { render } = require('react-dom');
+const videojs = require('video.js');
 
 module.exports = (path, fileName) => {
   const extAll = _.split(fileName, '.');
@@ -8,14 +9,18 @@ module.exports = (path, fileName) => {
   switch (ext) {
     case 'mp4':
       return (
-        <video controls src={path} type="video/mp4">
-          I'm sorry; your browser doesn't support HTML5 video in MP4 with H.264.
-        </video>
+        <div className="poop">
+          <p>{fileName}</p>
+          <video controls src={path} type="video/mp4" width="400" height="300">
+            I'm sorry; your browser doesn't support HTML5 video in MP4 with
+            H.264.
+          </video>
+        </div>
       );
       break;
     case 'webm':
       return (
-        <video controls src={path} type="video/webm">
+        <video controls src={path} type="video/webm" width="400" height="300">
           I'm sorry; your browser doesn't support HTML5 video in WebM with
           VP8/VP9.
         </video>
@@ -23,7 +28,13 @@ module.exports = (path, fileName) => {
       break;
     case 'mkv':
       return (
-        <video controls src={path} type="video/x-matroska, audio/x-matroska">
+        <video
+          controls
+          src={path}
+          type="video/x-matroska, audio/x-matroska"
+          width="400"
+          height="300"
+        >
           I'm sorry; your browser doesn't support HTML5 video in WebM with
           VP8/VP9.
         </video>
