71d86b1e32f8233d354e7f1278af7eff7ab109e3
с ноута <registoooqweqwe@gmail.com>
2019-06-21 11:58:53 +0300

    add fonts Comfortaa

--- src/clientSide/isVideo.js
+++ src/clientSide/isVideo.js
@@ -10,34 +10,40 @@ module.exports = (path, fileName) => {
     case 'mp4':
       return (
         <div className="poop">
-          <p>{fileName}</p>
           <video controls src={path} type="video/mp4" width="400" height="300">
             I'm sorry; your browser doesn't support HTML5 video in MP4 with
             H.264.
           </video>
+          <p className="naming">{fileName}</p>
         </div>
       );
       break;
     case 'webm':
       return (
-        <video controls src={path} type="video/webm" width="400" height="300">
-          I'm sorry; your browser doesn't support HTML5 video in WebM with
-          VP8/VP9.
-        </video>
+        <div className="poop">
+          <video controls src={path} type="video/webm" width="400" height="300">
+            I'm sorry; your browser doesn't support HTML5 video in WebM with
+            VP8/VP9.
+          </video>
+          <p className="naming">{fileName}</p>
+        </div>
       );
       break;
     case 'mkv':
       return (
-        <video
-          controls
-          src={path}
-          type="video/x-matroska, audio/x-matroska"
-          width="400"
-          height="300"
-        >
-          I'm sorry; your browser doesn't support HTML5 video in WebM with
-          VP8/VP9.
-        </video>
+        <div className="poop">
+          <video
+            controls
+            src={path}
+            type="video/x-matroska, audio/x-matroska"
+            width="400"
+            height="300"
+          >
+            I'm sorry; your browser doesn't support HTML5 video in WebM with
+            VP8/VP9.
+          </video>
+          <p className="naming">{fileName}</p>
+        </div>
       );
       break;
 
